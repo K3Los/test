2958b7d8072ea96a071e209fd8d8ca5e7bb54a55
K3Los <mrlos.com.kh@gmail.com>
2025-08-14 02:25:45 -0800

    Add files via upload

---
+++ 1script.js
@@ -0,0 +1,21 @@
+// var,  let, const;
+
+var id=101
+var name= "Jonh"
+var gender= "male"
+
+ console.log(`ID         : ${id}`)  
+ console.log(`My name is : ${name}`)  
+ console.log(`Gender     : ${gender}`)  
+
+// const data = (hello)=> {
+
+// }
+let a=10
+if(a<100){
+    console.log("a<100")
+}else if(a>100){
+    console.log("a>100")
+}else{
+    console.log("a == 100")
+}
